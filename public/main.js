328e0acc90dc09e0c1312ade79c356072cd44217
Björn Schmidtke <bs@pa1nd.de>
2020-08-04 22:22:07 -0400

    add own video as overlay

--- public/main.js
+++ public/main.js
@@ -1,4 +1,4 @@
-// let localVideo = document.getElementById('localVideo')
+let localVideo = document.getElementById('localVideo')
 let remoteVideo = document.getElementById('remoteVideo')
 let localStream
 let remoteUser
@@ -16,7 +16,9 @@ navigator.mediaDevices
     video: true,
   })
   .then(stream => {
-    // localVideo.srcObject = stream
+    localVideo.autoplay = true
+    localVideo.muted = true
+    localVideo.srcObject = stream
     localStream = stream
   })
 
